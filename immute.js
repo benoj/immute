9e1dd99ce88c735a32531fd428b0fbb18aa79dc6
benoj <ben.j.flowers@gmail.com>
2014-02-08 11:13:31 -0800

    setting object values correctly

--- immute.js
+++ immute.js
@@ -2,10 +2,11 @@ var _ = require('underscore');
 
 module.exports = function(objectDefinition){
 	var objectDefinitionKeys = Object.keys(objectDefinition);
+
 	return function(options){
 		var object = {};
 		_.each(objectDefinitionKeys,function(key){
-			object[key] = "blah";
+			object[key] = options[key];
 		});
 		return object;
 	};
--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect,
+	_ = require('underscore'),
 	immute = require('../immute');
 
 test('When object constructor is created, Then correct properties are set',function(){
@@ -8,4 +9,13 @@ test('When object constructor is created, Then correct properties are set',funct
 	var immutableKitty = new Kitty({name: "sheldon",cuteness: "10"});
 
 	expect(Object.keys(immutableKitty)).to.deep.equal(Object.keys(objectDefinition));
+});
+
+test('When object constructor is created, Then properties are set with correct values',function(){
+	var objectDefinition =  {name: "required", cuteness: "required"},
+		objectValues = {name: "sheldon",cuteness: "10"},
+		Kitty = immute(objectDefinition);
+	var immutableKitty = new Kitty(objectValues);
+
+	expect(_.pairs(immutableKitty)).to.deep.equal(_.pairs(objectValues));
 });
