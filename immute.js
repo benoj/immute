f2a5cfbb41cc90243f1cd12c2abb83941a36f8e1
benoj <ben.j.flowers@gmail.com>
2014-02-08 11:07:57 -0800

    refactoring using underscore

--- immute.js
+++ immute.js
@@ -1,5 +1,12 @@
-module.exports = function(){
-	return function(){
-		return {name:"bill",cuteness: "-2"};
+var _ = require('underscore');
+
+module.exports = function(objectDefinition){
+	var objectDefinitionKeys = Object.keys(objectDefinition);
+	return function(options){
+		var object = {};
+		_.each(objectDefinitionKeys,function(key){
+			object[key] = "blah";
+		});
+		return object;
 	};
 };
--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,11 @@
 var expect = require('chai').expect,
 	immute = require('../immute');
 
-test('hello',function(){
+test('When object constructor is created, Then correct properties are set',function(){
 	var objectDefinition =  {name: "required", cuteness: "required"},
 		Kitty = immute(objectDefinition);
 	
-	var immutableKitty = new Kitty("Sheldon","10");
+	var immutableKitty = new Kitty({name: "sheldon",cuteness: "10"});
 
 	expect(Object.keys(immutableKitty)).to.deep.equal(Object.keys(objectDefinition));
 });
